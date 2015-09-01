03892c8199eb6bc17ec3fbeeca751d24b084b51d
Wojciech A. Koszek <wojciech@koszek.com>
2015-08-31 23:09:13 -0700

    Accept input filename from the command line.

--- report.rb
+++ report.rb
@@ -4,6 +4,7 @@
 class Report
 	def initialize
 		@debug = 0
+		@filename_out = false
 	end
 
 	def dbg(*args)
@@ -16,7 +17,15 @@ class Report
 		ARGV.each do |arg|
 			if arg =~ /-d/ then
 				@debug = 1
+			else
+				@file_name_in = arg
 			end
+		end
+	end
+	def check_args
+		if not @file_name_in
+			print "usage!"
+			exit
 		end
 	end
 
@@ -33,7 +42,7 @@ class Report
 
 	def ex_count_run
 		total_run = 0
-		f = File.new("./typescript")
+		f = File.new(@file_name_in)
 		f.read().split("\n").each do |line|
 			if line =~ /^gtimeout/  or line =~ /^timeout/ then
 				self.dbg "#{line}\n"
@@ -67,6 +76,8 @@ end
 def main
 	r = Report.new()
 	r.read_args()
+	r.check_args()
+
 
 	cnt_total = r.ex_count_total()
 	cnt_run = r.ex_count_run()
