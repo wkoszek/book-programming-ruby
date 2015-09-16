159f6584bd889c74639e3efed7c5c2975d329140
Wojciech A. Koszek <wojciech@koszek.com>
2015-09-16 00:53:37 -0700

    Add chmod on the file.

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -10,6 +10,7 @@ git clone https://github.com/wkoszek/travis_deploy.git
 ls -la
 ls -la travis_deploy
 openssl aes-256-cbc -K $encrypted_a95d9d0517a4_key -iv $encrypted_a95d9d0517a4_iv -in travis_deploy/book-programming-ruby.enc -out book-programming-ruby.key -d
+chmod 600 book-programming-ruby.key
 ssh-add book-programming-ruby.key
 
 git clone git@github.com:wkoszek/book-programming-ruby.git
