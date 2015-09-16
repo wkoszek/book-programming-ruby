be27bd95518a3bb28016d243700c84c8dcca33d0
Wojciech A. Koszek <wojciech@koszek.com>
2015-09-16 00:16:04 -0700

    Travis out. build

---
+++ scripts/report.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git clone https://github.com/wkoszek/travis_deploy.git
+openssl aes-256-cbc -K $encrypted_a95d9d0517a4_key -iv $encrypted_a95d9d0517a4_iv -in travis_deploy/book-programming-ruby.enc -out book-programming-ruby.key -d
+ssh-add book-programming-ruby.key
+git clone git@github.com:wkoszek/book-programming-ruby.git
+cd book-programming-ruby
+git co gh-pages
+mv ../out.${MODE} out.${MODE}.${TRAVIS_BUILD_NUMBER}
+git add *
+git commit -m "Travis out.${MODE} build ${TRAVIS_BUILD_NUMBER}"
+git push
