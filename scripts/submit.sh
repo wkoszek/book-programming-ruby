a64a2d3e81486d4303197a00727aa8b5c6c89db9
Wojciech A. Koszek <wojciech@koszek.com>
2015-11-06 00:50:39 -0500

    Fix the deployment script.

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -4,27 +4,23 @@ set -x
 
 git config --global user.email "wojciech@koszek.com"
 git config --global user.name "Wojciech A. Koszek, T"
-git config --global push.default matching
+git config --global push.default simple
 
 pwd
 git clone https://github.com/wkoszek/travis_deploy.git
 ls -la
 ls -la travis_deploy
 openssl aes-256-cbc -K $encrypted_a95d9d0517a4_key -iv $encrypted_a95d9d0517a4_iv -in travis_deploy/book-programming-ruby.enc -out book-programming-ruby.key -d
+
 chmod 600 book-programming-ruby.key
 ssh-add book-programming-ruby.key
 
 git clone git@github.com:wkoszek/book-programming-ruby.git
 cd book-programming-ruby
-#git remote add origin https://github.com/wkoszek/book-programming-ruby
-git checkout -B gh-pages
-#git push --set-upstream origin gh-pages
-#git pull
-#git branch --set-upstream-to=origin/gh-pages gh-pages
-#git pull
+git checkout -b gh-pages --track origin/gh-pages
 
 mkdir -p results
 mv ../out.${MODE} results/${TRAVIS_BUILD_NUMBER}.out.${MODE}
 git add results/${TRAVIS_BUILD_NUMBER}.out.${MODE}
-git commit -m ddddd
-git push -f # --all # origin master
+git commit -m "New report `date`"
+git push origin gh-pages
